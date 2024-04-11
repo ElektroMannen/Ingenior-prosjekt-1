42fc6f8f8c3e584ab742dafd7e9da7f05d57225d
ElektroMannen <ottarmr@stud.ntnu.no>
2024-04-11 09:49:00 +0200

    ..

--- Code/BilESP/BilESP.ino
+++ Code/BilESP/BilESP.ino
@@ -5,8 +5,25 @@ const char ssid[] = "NTNU-IOT";
 const char passord[] = "";
 
 
-void setup(){
+void wifiConnect(){
+// Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
 
+void setup(){
+    wifiConnect();
 }
 void loop(){
 
