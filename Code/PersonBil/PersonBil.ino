31ad28415b39573e905172414ad35a05dee189df
ElektroMannen <ottarmr@stud.ntnu.no>
2024-04-11 08:49:15 +0200

    First publish of car files

---
+++ Code/PersonBil/PersonBil.ino
@@ -0,0 +1,49 @@
+
+
+
+/*Takes inn direction of which way the car will drive. The diretion is based on norms for keyboard for movement.
+Diredtions avalible are W for forwards, S for backwards, A for turn right, D for turn left, Q for curve towards left, E for curve towards right*/
+void drive(char direction){
+
+    switch (direction){
+        //Goes forward
+        case 87:
+
+            break;
+        //Goes Backwards
+        case 83:
+
+            break;
+        //Turn right
+        case 65:
+
+            break;
+        //Turns left
+        case 68:
+
+            break;
+        //Curves left
+        case 81:
+
+            break;
+        //Curves right
+        case 69:
+
+            break;
+
+        default:
+            break;
+    }
+}
+
+//Gets MQTT controller topic and 
+int getControlls(){
+    
+}
+
+void setup(){
+
+}
+void loop(){
+
+}
