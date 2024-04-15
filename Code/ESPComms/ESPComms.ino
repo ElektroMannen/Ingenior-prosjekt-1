8b5a75f181b91e784090a7baf0c929a6d5531e5f
Youmayu <goldenmaker2000@gmail.com>
2024-04-15 12:41:07 +0200

    Message structure setup

--- Code/ESPComms/ESPComms.ino
+++ Code/ESPComms/ESPComms.ino
@@ -3,6 +3,21 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+//Address of cars:
+uint8_t car1Address[] = {0x24, 0x6F, 0x28, 0x1D, 0x6C, 0x4C};
+uint8_t car2Address[] = {0x24, 0x6F, 0x28, 0x1D, 0x6C, 0x4C};
+
+//Address of city modules:
+uint8_t city1Address[] = {0x24, 0x6F, 0x28, 0x1D, 0x6C, 0x4C};
+uint8_t city2Address[] = {0x24, 0x6F, 0x28, 0x1D, 0x6C, 0x4C};
+//Remember to change the addresses to the correct ones
+
+//Message structure
+typedef struct struct_message {
+  int id;
+  int data;
+  String message;
+} struct_message;
 
 void setup(){
 //Put some code here
