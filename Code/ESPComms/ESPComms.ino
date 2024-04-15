4c02fb1cb1def9eb2c5044427a14a4f7a8f2a9e0
Youmayu <goldenmaker2000@gmail.com>
2024-04-15 12:15:57 +0200

    Setting up comms

--- Code/ESPComms/ESPComms.ino
+++ Code/ESPComms/ESPComms.ino
@@ -1,5 +1,7 @@
 // In order to use this code you'll need to know the MAC addresses of your ESP32s, if you don't know them you can use the code in the "GetMacAddress" folder to find them.
 
+#include <esp_now.h>
+#include <WiFi.h>
 
 
 void setup(){
