807d24a894102658b739d382ea8f61cba1c17227
wltwt <wltwt@proton.me>
2024-04-18 17:10:54 +0200

    working test db

--- Code/Server/docker/mariadb-config/initdb.sql
+++ Code/Server/docker/mariadb-config/initdb.sql
@@ -2,8 +2,10 @@ DROP DATABASE testdb;
 CREATE DATABASE IF NOT EXISTS testdb;
 USE testdb;
 
+-- opprett tabeller
+
 CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	address VARCHAR(255) NOT NULL,
@@ -16,25 +18,48 @@ CREATE TABLE vehicles (
 	owner_id INT
 ) ENGINE=InnoDB;
 
-CREATE TABLE drivingscore (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE drivescores (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	ts TIMESTAMP,
 	score FLOAT NOT NULL,
-	vehicle_id INT UNSIGNED NOT NULL,
-	CONSTRAINT `fk_drivingscore_vehicles`
-      FOREIGN KEY (vehicle_id) REFERENCES vehicles (id)
+	user_id INT UNSIGNED,
+	CONSTRAINT `fk_drivescores_users`
+      FOREIGN KEY (user_id) REFERENCES users (id)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE rentrecords (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	start_date DATE NOT NULL,
+	end_date DATE,
+	average_score FLOAT,
+	user_id INT UNSIGNED NOT NULL,
+    CONSTRAINT `fk_rentrecords_users`
+      FOREIGN KEY (user_id) REFERENCES users (id)
       ON DELETE CASCADE
       ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
 
+INSERT INTO users (name, email, address) VALUES
+	('Arne Midjo', 'arne.midjo@mail.no', 'ntnubakken 12C'),
+	('Mrne Aidjo', 'mrne@gmail.com', 'utnudalen 18C');
+
+INSERT INTO drivescores (score, user_id) VALUES 
+	(134.82, 1),
+	(111.82, 1);
+
 INSERT INTO vehicles (class) VALUES ('Garbage collector');
 INSERT INTO vehicles (class, owner_id) VALUES ('Personal',1);
 INSERT INTO vehicles (class) VALUES ('Street sweeper');
-INSERT INTO drivingscore (score, vehicle_id) VALUES 
-	(134.82, LAST_INSERT_ID()),
-	(111.82, 1);
 
-INSERT INTO users (name, email, address) VALUES
-	('Arne Midjo', 'arne.midjo@mail.no', 'ntnubakken 12C');
+
+/* eksempel på å finne alle scores knyttett 
+ * til en bruker og lagre gjennomsnittet */
+
+INSERT INTO rentrecords (start_date, average_score, user_id)
+SELECT '2024-03-21', AVG(score), 1 
+FROM drivescores
+WHERE user_id = 1;
 
 
