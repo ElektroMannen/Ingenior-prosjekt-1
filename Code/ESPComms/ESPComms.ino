ba1ce1bc8dac11ede44bfe64e1bd6ef4bb0cb70e
Youmayu <goldenmaker2000@gmail.com>
2024-04-29 14:04:49 +0200

    Finished cleanup

--- Code/ESPComms/ESPComms.ino
+++ Code/ESPComms/ESPComms.ino
@@ -39,9 +39,8 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 
 void setup() {
   Serial.begin(115200);
-  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-
+  mesh.setDebugMsgTypes( ERROR | STARTUP );
+  
   mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
