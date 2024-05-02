f379f746482f363850edf20bbc5045780a4ff81a
Youmayu <goldenmaker2000@gmail.com>
2024-05-02 11:19:32 +0200

    It fucking works now, fuck intellisense, 3 hours wasted

--- Code/ESPComms/GetMacAddress/GetMacAddress.ino
+++ Code/ESPComms/GetMacAddress/GetMacAddress.ino
@@ -1,10 +1,11 @@
+
 // Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
 
-#include "WiFi.h"
+#include <WiFi.h>
  
 void setup(){
   Serial.begin(115200);
-  WiFi.mode(WIFI_MODE_STA);
+  WiFi.mode(WIFI_AP_STA);
   Serial.println(WiFi.macAddress());
 }
  
