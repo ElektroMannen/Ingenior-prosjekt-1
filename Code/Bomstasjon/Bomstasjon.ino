ee633937ae9e55e48ffb782ab62e2de5efd9b9f4
Youmayu <103046178+Youmayu@users.noreply.github.com>
2024-05-16 10:49:32 +0200

    Ultrasonic sensor setup

--- Code/Bomstasjon/Bomstasjon.ino
+++ Code/Bomstasjon/Bomstasjon.ino
@@ -7,11 +7,26 @@ const int echoPin = 19;
 
 float duration, distance;
 
+elapsedMicros offTimer;
+elapsedMicros onTimer;
+
 void setup(){
     Serial.begin(115200);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 void loop(){
-    
+    digitalWrite(trigPin, LOW);
+    if (offTimer >= 2){
+        offTimer -= 2;
+        digitalWrite(trigPin, HIGH);
+    }
+    if (onTimer >= 12){
+        onTimer -= 12;
+        digitalWrite(trigPin, LOW);
+    }
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    Serial.print("Distance: ");
+    Serial.println(distance);
 }
