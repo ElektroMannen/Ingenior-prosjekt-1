4bbfd234021fcfc5163df46707153851062424e9
ElektroMannen <ottarmr@stud.ntnu.no>
2024-05-23 09:08:38 +0200

    Speed reading added

--- Code/PersonBil/PersonBil.ino
+++ Code/PersonBil/PersonBil.ino
@@ -1,3 +1,13 @@
+/*
+TO DO:
+- Legge inn score
+- Legge inn IR kommuniksajon
+- Sende data opp til mqtt 
+DONE:
+- Legge inn fart
+*/
+
+
 #include <Wire.h>
 #include <Zumo32U4.h>
 
@@ -10,12 +20,14 @@ Zumo32U4ButtonC buttonC;
 Zumo32U4OLED display;
 Zumo32U4Buzzer buzzer;
 Zumo32U4IMU imu;
+Zumo32U4IRPulses IR;
 
 
 
 int warning = 0;
 int disp = 0;
 int32_t speed = 100;
+int32_t speedMulti = 0;
 int32_t messureSpeed;
 int32_t pitch, pitchCalibrate;
 int32_t powerRemaining;
@@ -28,17 +40,19 @@ bool isMoving;
 //Distance variables
 long distanceL = 0;
 long distanceR = 0;
-long distance = 0;
+int32_t oldDistance = 0;
 unsigned long aksMillis = 0;
 unsigned long oldUiTime = 0;
+unsigned long speedMillis = 0;
 
 struct RecievedData{
   int32_t drive, driverLevel, driverScore;
   bool warning;
-
 };
+
 struct TransmittData{
-  int32_t distance, maxSpeed, batteryHealth, batteryVoltage;
+  int32_t maxSpeed, batteryHealth, batteryVoltage, driverScore, distance;
+  float speed;
 };
 
 RecievedData data;
@@ -54,11 +68,11 @@ void startDisp(){
   display.gotoXY(0, 2);
   display.print("|--------|");
   display.setLayout21x8();
-  buzzer.playNote(NOTE_E(4),200,10);
+  /*buzzer.playNote(NOTE_E(4),200,10);
   delay(100);
   buzzer.playNote(NOTE_G(4),200,10);
   delay(100);
-  buzzer.playNote(NOTE_G(5),200,10);
+  buzzer.playNote(NOTE_G(5),200,10);*/
 }
 
 
@@ -73,26 +87,20 @@ void uiHandler(){
   display.print(char(data.drive));
   display.gotoXY(0, 3);
   display.print("Warning: ");
-  if(data.warning){
-  display.print("True");
-  }
-  else{
-  display.print("False");
-  }
+  if(data.warning){display.print("True");}
+  else{display.print("False");}
   display.gotoXY(0, 4);
   display.print("Distance: ");
   display.print(sensorData.distance);
   display.print("cm");
   display.gotoXY(0, 5);
   display.print("Speed: ");
-  display.print(sensorData.distance/500);
+  display.print(sensorData.speed);
+  display.print("cm/s");
   display.gotoXY(0, 6);
   display.print("Power: ");
-  display.print(powerRemaining);
+  display.print(sensorData.batteryVoltage);
   display.print("%");
-  display.gotoXY(0, 7);
-  display.print("Pitch: ");
-  display.print(pitch);
   oldUiTime = millis();
   }
 }
@@ -106,8 +114,8 @@ void drive(int32_t direction){
         //Goes forward
         case 87:
             motors.setSpeeds(speed,speed);
-            if((millis() - aksMillis) > 500 && (speed < 200)){
-              speed += 25;
+            if((millis() - aksMillis) > 500 && (speed < 10)){
+              speed += 10;
               aksMillis = millis();
             }
             isMoving = true;
@@ -115,15 +123,15 @@ void drive(int32_t direction){
         //Goes Backwards
         case 83:
             motors.setSpeeds(-1*speed,-1*speed);
-            if((millis() - aksMillis) > 500 && (speed < 200)){
-              speed += 25;
+            if((millis() - aksMillis) > 500 && (speed < 10)){
+              speed += 10;
               aksMillis = millis();
             isMoving = true;
             }
             break;
         //Turn right
         case 65:
-            motors.setSpeeds(-100,100);
+            motors.setSpeeds(-speed/2,speed/2);
             aksMillis = millis();
             isMoving = true;
             break;
@@ -135,28 +143,15 @@ void drive(int32_t direction){
             break;
         //Curves left
         case 81:
-            motors.setSpeeds(100,200);
+            motors.setSpeeds(speed,speed*2);
             aksMillis = millis();
             isMoving = true;
             break;
         //Curves right
         case 69:
-            motors.setSpeeds(200,100);
+            motors.setSpeeds(speed*2,speed);
             aksMillis = millis();
             isMoving = true;
-            break;
-
-          //Set speed by using 1,2,3
-        case 49:
-           
-            break;
-
-          case 50:
-            
-            break;
-
-          case 51:
-            
             break;
 
         //Stops the car when no input
@@ -184,12 +179,18 @@ void getData(){
   }
   //Get voltage remaining in % only when car is not moving 2.7V -> 6V
   if(isMoving != true){
-  powerRemaining = map(readBatteryMillivolts(),2700, 6000, 0, 100);
+    sensorData.batteryVoltage = map(readBatteryMillivolts(),2700, 6000, 0, 100);
   }
-
+  // Gets distance
   distanceL = distanceL + encoders.getCountsAndResetLeft();
   distanceR = distanceR + encoders.getCountsAndResetRight();
   sensorData.distance =  round((((((distanceL + distanceR)/2))/909.7))*10);
+
+  if((millis() - speedMillis) > 200){
+    sensorData.speed = ((sensorData.distance - oldDistance)/0.2);
+    oldDistance = sensorData.distance;
+    speedMillis = millis();
+  }
 }
 
 /*Code executon starts
@@ -201,7 +202,7 @@ void setup(){
   imu.enableDefault();
   imu.readGyro();
   pitchCalibrate = imu.g.y;
-  sensorData.distance = 10;
+  Wire.onRequest(sendI2C_Data);
   startDisp();
   delay(2000);
   display.clear();
@@ -218,4 +219,6 @@ void loop(){
 }
 
 
-
+void sendI2C_Data(){
+  Wire.write((byte*)&sensorData, sizeof(sensorData)); // Les hele structen fra I2C-bussen
+}
