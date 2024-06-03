163552a34fa96e4291f57978cb2001b724934804
wltwt <wltwt@proton.me>
2024-06-03 18:21:50 +0200

    Implement charging station

--- Code/Server/Serial/main.py
+++ Code/Server/Serial/main.py
@@ -1,46 +1,28 @@
 import paho.mqtt.client as mqtt
 import serial
-import time
 
 ser = serial.Serial('/dev/ttyS0', 115200) #9600 is baud rate(must be same with that of NodeMCU)
 ser.flush()
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("info/test")
 
 
 def on_subscribe(client, userdata, mid, reason_code_list, properties):
-    # Since we subscribed only for a single channel, reason_code_list contains
-    # a single entry
     if reason_code_list[0].is_failure:
         print(f"Broker rejected you subscription: {reason_code_list[0]}")
     else:
         print(f"Broker granted the following QoS: {reason_code_list[0].value}")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
 def on_publish(client, userdata, mid, reason_code, properties):
-    # reason_code and properties will only be present in MQTTv5. It's always unset in MQTTv3
     try:
         userdata.remove(mid)
     except KeyError:
         print("on_publish() is called with a mid not present in unacked_publish")
-        print("This is due to an unavoidable race-condition:")
-        print("* publish() return the mid of the message sent.")
-        print("* mid from publish() is added to unacked_publish by the main thread")
-        print("* on_publish() is called by the loop_start thread")
-        print("While unlikely (because on_publish() will be called after a network round-trip),")
-        print(" this is a race-condition that COULD happen")
-        print("")
-        print("The best solution to avoid race-condition is using the msg_info from publish()")
-        print("We could also try using a list of acknowledged mid rather than removing from pending list,")
-        print("but remember that mid could be re-used !")
     
 
 unacked_publish = set()
@@ -52,9 +34,6 @@ mqttc.connect("localhost")
 
 mqttc.user_data_set(unacked_publish)
 #mqttc.loop_start()
-
-
-# Due to race-condition described above, the following way to wait for all publish is safer
 
 
 mqttc.loop_start()
