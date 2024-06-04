7b0b92d43ce7e4a5b53bbf0717de40febe45c88a
zuzannakalawska <145001903+zuzannakalawska@users.noreply.github.com>
2024-06-04 10:37:02 +0200

    Update ladevakt_med_sensor_3_stk.ino
    added user and station ID

--- Code/ladevakt_med_sensor_3_stk/ladevakt_med_sensor_3_stk.ino
+++ Code/ladevakt_med_sensor_3_stk/ladevakt_med_sensor_3_stk.ino
@@ -7,7 +7,7 @@
 const char* ssid = "NTNU-IOT"; //nettverksnavn
 const char* password = ""; //passord 
 
-const char* mqtt_server = "10.25.19.44";
+const char* mqtt_server = "10.25.19.44"; //MQTT 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -28,7 +28,7 @@ const int echoPin3=4;
 const int ledPin3=19;
 const int lightPin3=35;
 
-//variabel for sensorverdi
+//variabler for sensorverdi
 float distance1, duration1;
 float distance2, duration2;
 float distance3, duration3;
@@ -94,21 +94,17 @@ void sensorSetup(){
   pinMode(ledPin3, OUTPUT);
 }
 
-
+//MQTT tilkobling, loop som kjører dersom MQTT ikke er tilkoblet 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
-      // Subscribe
       client.subscribe("esp32/output");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -126,6 +122,7 @@ void setup(){
   sensorSetup();
 }
 
+//henter ut sensorverdi fra lyssensor 1
 void getLightValue1(){
   analogValue1 = analogRead(lightPin1);
 
@@ -133,6 +130,7 @@ void getLightValue1(){
   Serial.println(analogValue1);
 }
 
+//henter ut sensorverdi fra lyssensor 2
 void getLightValue2(){
   analogValue2 = analogRead(lightPin2);
 
@@ -140,6 +138,7 @@ void getLightValue2(){
   Serial.println(analogValue2);
 }
 
+//henter ut sensorverdi fra lyssensor 3
 void getLightValue3(){
   analogValue3 = analogRead(lightPin3);
 
@@ -147,8 +146,10 @@ void getLightValue3(){
   Serial.println(analogValue3);
 }
 
+//oppdaterer ladekapasitet i ladestasjon 1
 void powerSupply1(){
-  getLightValue1();
+  getLightValue1(); //henter sensorverdi
+  //ladekapasiteten blir oppdatert basert på været
   if(weatherValue1=="Sol"){
     powerSupplyValue1+=(analogValue1/8);
   }
@@ -158,11 +159,13 @@ void powerSupply1(){
   else{
     powerSupplyValue1+=(analogValue1/32);
   }
-  client.publish("esp32/powersupply/value1", String(powerSupplyValue1).c_str());
+  client.publish("esp32/powersupply/value1", String(powerSupplyValue1).c_str()); //publiserer verdien 
 }
 
+//oppdaterer ladekapasitet i ladestasjon 2
 void powerSupply2(){
-  getLightValue2();
+  getLightValue2(); //henter sensorverdi
+  //ladekapasiteten blir oppdatert basert på været
   if(weatherValue2=="Sol"){
     powerSupplyValue2+=(analogValue2/8);
   }
@@ -172,11 +175,13 @@ void powerSupply2(){
   else{
     powerSupplyValue2+=(analogValue2/32);
   }
-  client.publish("esp32/powersupply/value2", String(powerSupplyValue2).c_str());
+  client.publish("esp32/powersupply/value2", String(powerSupplyValue2).c_str()); //publiserer verdien
 }
 
+//oppdaterer ladekapasitet i ladestasjon 3
 void powerSupply3(){
-  getLightValue3();
+  getLightValue3(); //henter sensorverdi
+  //ladekapasiteten blir oppdatert basert på været
   if(weatherValue3=="Sol"){
     powerSupplyValue3+=(analogValue3/8);
   }
@@ -186,33 +191,38 @@ void powerSupply3(){
   else{
     powerSupplyValue3+=(analogValue3/32);
   }
-  client.publish("esp32/powersupply/value3", String(powerSupplyValue3).c_str());
+  client.publish("esp32/powersupply/value3", String(powerSupplyValue3).c_str()); //publiserer verdien
 }
 
+//funksjon for lading ved ladestasjon 1
 void charging1(){
   unsigned long currentChargingMillis1=millis();
   if (currentChargingMillis1-previousChargingMillis1 >= chargingInterval){
     previousChargingMillis1=currentChargingMillis1;
-    powerSupplyValue1-=250;
-    client.publish("esp32/powersupply/value1", String(powerSupplyValue1).c_str());
+    powerSupplyValue1-=250; //ladekapasiteten blir tappet med 250 per tidsintervall
+    client.publish("esp32/powersupply/value1", String(powerSupplyValue1).c_str()); //publiserer den oppdaterte verdien
     if (powerSupplyValue1<250){
-      String chargingMessage="Lading ikke tilgjengelig";
-      powerSupplyValue1=0;
-      client.publish("esp32/powersupply/value1", String(chargingMessage).c_str());
+      String chargingMessage="Lading ikke tilgjengelig"; //lading er ikke tilgjengelig da ladestasjonen ikke har nok kapasitet
+      if (powerSupplyValue1<=0){
+        powerSupplyValue1=0; //setter verdien til 0 slik at det ikke blir negativ kapasitet
+      }
+      client.publish("esp32/powersupply/value1", String(chargingMessage).c_str()); //publiserer den oppdaterte verdien
     }
   }
 }
-
+//funksjon for lading ved ladestasjon 1
 void charging2(){
   unsigned long currentChargingMillis2=millis();
   if (currentChargingMillis2-previousChargingMillis2 >= chargingInterval){
     previousChargingMillis2=currentChargingMillis2;
-    powerSupplyValue2-=250;
-    client.publish("esp32/powersupply/value2", String(powerSupplyValue2).c_str());
+    powerSupplyValue2-=250; //ladekapasiteten blir tappet med 250 per tidsintervall
+    client.publish("esp32/powersupply/value2", String(powerSupplyValue2).c_str()); //publiserer den oppdaterte verdien
     if (powerSupplyValue2<250){
-      String chargingMessage="Lading ikke tilgjengelig";
-      powerSupplyValue2=0;
-      client.publish("esp32/powersupply/value2", String(chargingMessage).c_str());
+      String chargingMessage="Lading ikke tilgjengelig"; //lading er ikke tilgjengelig da ladestasjonen ikke har nok kapasitet
+      if (powerSupplyValue2<=0){
+        powerSupplyValue2=0; //setter verdien til 0 slik at det ikke blir negativ kapasitet
+      }
+      client.publish("esp32/powersupply/value2", String(chargingMessage).c_str()); //publiserer den oppdaterte verdien
     }
   }
 }
@@ -221,21 +231,25 @@ void charging3(){
   unsigned long currentChargingMillis3=millis();
   if (currentChargingMillis3-previousChargingMillis3 >= chargingInterval){
     previousChargingMillis3=currentChargingMillis3;
-    powerSupplyValue3-=250;
-    client.publish("esp32/powersupply/value3", String(powerSupplyValue3).c_str());
+    powerSupplyValue3-=250; //ladekapasiteten blir tappet med 250 per tidsintervall
+    client.publish("esp32/powersupply/value3", String(powerSupplyValue3).c_str()); //publiserer den oppdaterte verdien
     if (powerSupplyValue3<250){
-      String chargingMessage="Lading ikke tilgjengelig";
-      powerSupplyValue3=0;
-      client.publish("esp32/powersupply/value3", String(chargingMessage).c_str());
+      String chargingMessage="Lading ikke tilgjengelig"; //lading er ikke tilgjengelig da ladestasjonen ikke har nok kapasitet
+      if (powerSupplyValue3<=0){
+        powerSupplyValue3=0; //setter verdien til 0 slik at det ikke blir negativ kapasitet
+      }
+      client.publish("esp32/powersupply/value3", String(chargingMessage).c_str()); //publiserer den oppdaterte verdien
     }
   }
 }
 
+//værstasjon på ladestasjon 1
 void weatherStation1(){
   unsigned long currentSensorMillis1=millis();
   if (currentSensorMillis1-previousSensorMillis1 > sensorInterval){
     previousSensorMillis1=currentSensorMillis1;
-    powerSupply1();
+    powerSupply1(); 
+    //bestemmer værmeldinga basert på lyssensorverdien
     if (analogValue1>= 2200){
       weatherValue1="Sol";
     }
@@ -245,15 +259,17 @@ void weatherStation1(){
     else{
       weatherValue1="Overskyet";
     }
-    client.publish("esp32/weather1", String(weatherValue1).c_str());
+    client.publish("esp32/weather1", String(weatherValue1).c_str()); //publiserer værmelding
   }
 }
 
+//værstasjon på ladestasjon 2
 void weatherStation2(){
   unsigned long currentSensorMillis2=millis();
   if (currentSensorMillis2-previousSensorMillis2>sensorInterval){
     previousSensorMillis2=currentSensorMillis2;
     powerSupply2();
+    //bestemmer værmeldinga basert på lyssensorverdien
     if (analogValue2>= 2200){
       weatherValue2="Sol";
     }
@@ -263,15 +279,17 @@ void weatherStation2(){
     else{
       weatherValue2="Overskyet";
     }
-  client.publish("esp32/weather2", String(weatherValue2).c_str());
+  client.publish("esp32/weather2", String(weatherValue2).c_str()); //publiserer værmelding
   }
 }
 
+//værstasjon på ladestasjon 3
 void weatherStation3(){
   unsigned long currentSensorMillis3=millis();
   if (currentSensorMillis3-previousSensorMillis3>sensorInterval){
     previousSensorMillis3=currentSensorMillis3;
     powerSupply3();
+    //bestemmer værmeldinga basert på lyssensorverdien
     if (analogValue3>= 2200){
       weatherValue3="Sol";
     }
@@ -281,10 +299,11 @@ void weatherStation3(){
     else{
       weatherValue3="Overskyet";
     }
-    client.publish("esp32/weather3", String(weatherValue3).c_str());
+    client.publish("esp32/weather3", String(weatherValue3).c_str()); //publiserer værmelding
   }
 }
 
+//avlesing av avstandssensor på ladestasjon 1
 void sensorRead1(){
   unsigned long currentMillis1 = millis();
   if (currentMillis1 - previousMillis1 >= interval) {
@@ -300,20 +319,20 @@ void sensorRead1(){
     Serial.print("Distance1: ");
     Serial.println(distance1);
   }
-  // Kontroller LED basert på avstanden
+  //kontrollerer LED basert på avstanden
   if(distance1 < sensor_threshold){
     digitalWrite(ledPin1, HIGH);
-    ledValue1 = "Opptatt";
+    ledValue1 = "Opptatt, User ID: 1, Station ID: 1";
     charging1();
   } else {
     digitalWrite(ledPin1, LOW);
-    ledValue1 = "Ledig";
+    ledValue1 = "Ledig, User ID: 1, Station ID: 1";
   }
 
-  //publish led value
-  client.publish("esp32/led/value1", String(ledValue1).c_str());
+  client.publish("esp32/led/value1", String(ledValue1).c_str()); //publiserer LED verdi
 }
 
+//avlesing av avstandssensor på ladestasjon 2
 void sensorRead2(){
   unsigned long currentMillis2 = millis();
   if (currentMillis2 - previousMillis2 >= interval) {
@@ -329,20 +348,20 @@ void sensorRead2(){
     Serial.print("Distance2: ");
     Serial.println(distance2);
   }
-  // Kontroller LED basert på avstanden
+  //kontrollerer LED basert på avstanden
   if(distance2 < sensor_threshold){
     digitalWrite(ledPin2, HIGH);
-    ledValue2 = "Opptatt";
+    ledValue2 = "Opptatt, User ID: 2, Station ID: 2";
     charging2();
   } else {
     digitalWrite(ledPin2, LOW);
-    ledValue2 = "Ledig";
+    ledValue2 = "Ledig, User ID: 2, Station ID: 2";
   }
 
-  //publish led value
-  client.publish("esp32/led/value2", String(ledValue2).c_str());
+  client.publish("esp32/led/value2", String(ledValue2).c_str()); //publiserer LED verdi
 }
 
+//avlesing av avstandssensor på ladestasjon 3
 void sensorRead3(){
   unsigned long currentMillis3 = millis();
   if (currentMillis3 - previousMillis3 >= interval) {
@@ -358,20 +377,20 @@ void sensorRead3(){
     Serial.print("Distance3: ");
     Serial.println(distance3);
   }
-  // Kontroller LED basert på avstanden
+  //kontrollerer LED basert på avstanden
   if(distance3 < sensor_threshold){
     digitalWrite(ledPin3, HIGH);
-    ledValue3 = "Opptatt";
+    ledValue3 = "Opptatt, User ID: 3, Station ID: 3";
     charging3();
   } else {
     digitalWrite(ledPin3, LOW);
-    ledValue3 = "Ledig";
+    ledValue3 = "Ledig, User ID: 3, Station ID: 3";
   }
 
-  //publish led value
-  client.publish("esp32/led/value3", String(ledValue3).c_str());
+  client.publish("esp32/led/value3", String(ledValue3).c_str()); //publiserer LED verdi
 }
 
+//kaller alle nødvendige funksjoner i loop
 void loop() {
   if (!client.connected()) {
     reconnect();
